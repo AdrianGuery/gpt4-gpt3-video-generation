57153b93cd664d2950dd562e4d2d5a81f63aa9f1
Adrian Guery <adrian.guery@toolpad.fr>
2023-03-20 20:33:09 +0100

    feat - install necessary packages 2

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,1 +1,5 @@
-declare module '*.scss';
+declare module "*.scss";
+declare module "react-code-snippet";
+declare module "react-code-prettify";
+declare module "prettier/standalone";
+declare module "prettier/parser-babel";
